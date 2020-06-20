699b3f541b05ed99353fbcb607427886d6371134
shubhamkhaitan <65844445+shubhamkhaitan@users.noreply.github.com>
2020-06-20 12:56:58 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -10,8 +10,8 @@
 (function (window) {
   var helloSpeaker = {};
   var speakWord = "Hello";
-  helloSpeaker.speak = function () {
-    console.log(speakWord + " " + helloSpeaker.names);
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
   window.helloSpeaker = helloSpeaker;
 })(window);
